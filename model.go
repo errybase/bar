ee3f7fd9a7face67d4ec45189985fa736b9b192b
tdd28 <tdd2810@gmail.com>
2025-11-07 18:47:47 +0700

    feat: add ModelWithTimestamps type

---
+++ model.go
@@ -0,0 +1,20 @@
+package bar
+
+import (
+	"context"
+	"time"
+
+	"github.com/uptrace/bun"
+)
+
+var _ bun.BeforeUpdateHook = (*ModelWithTimestamps)(nil)
+
+type ModelWithTimestamps struct {
+	CreatedAt time.Time `bun:"created_at,nullzero,notnull,default:current_timestamp"`
+	UpdatedAt time.Time `bun:"updated_at,nullzero,notnull,default:current_timestamp"`
+}
+
+func (m *ModelWithTimestamps) BeforeUpdate(ctx context.Context, query *bun.UpdateQuery) error {
+	m.UpdatedAt = time.Now()
+	return nil
+}
